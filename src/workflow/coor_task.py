c657ba718d5272477c227762554ea68303bb9433
george <george@seamlessai.ntesmail.com>
2025-06-04 22:09:52 +0800

    fix

--- src/workflow/coor_task.py
+++ src/workflow/coor_task.py
@@ -79,7 +79,7 @@ async def publisher_node(state: State) -> Command[Literal["agent_proxy", "agent_
         if agent == "FINISH":
             goto = "__end__"
             logger.info("Workflow completed \n")
-            cache.restore_node(state["workflow_id"], goto, state["initialized"])
+            cache.restore_node(state["workflow_id"], goto, state["initialized"], state["user_id"])
             return Command(goto=goto, update={"next": goto})
         elif agent != "agent_factory":
             goto = "agent_proxy"
@@ -87,7 +87,7 @@ async def publisher_node(state: State) -> Command[Literal["agent_proxy", "agent_
             goto = "agent_factory"
         logger.info(f"publisher delegating to: {agent} \n")
         
-        cache.restore_node(state["workflow_id"], agent, state["initialized"])
+        cache.restore_node(state["workflow_id"], agent, state["initialized"], state["user_id"])
         
     elif state["work_mode"] in ["production", "polish"]:
         # todo add polish history
@@ -120,7 +120,7 @@ async def agent_proxy_node(state: State) -> Command[Literal["publisher","__end__
     response = await agent.ainvoke(state)
     
     if state["work_mode"] == "launch":
-        cache.restore_node(state["workflow_id"], _agent, state["initialized"])
+        cache.restore_node(state["workflow_id"], _agent, state["initialized"], state["user_id"])
     elif state["work_mode"] == "production":
         cache.update_stack(state["workflow_id"], state["user_id"])
 
